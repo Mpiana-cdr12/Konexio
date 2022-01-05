047e3cf4a84a25d4056bb29a9757f4811bfe7e6d
Mpiana-cdr12 <mgmtdavidmpiana@gmail.com>
2022-01-05 17:51:41 +0100

    jour 5 exo

--- Jour05/exo1.js
+++ Jour05/exo1.js
@@ -1,6 +1,16 @@
-function calculate(number, operator,number1){ // Créer une fonction calculate 
+// 01 - Calculator 
+function calculate(number, operator,number1){ // Créer une fonction calculate qui recevra 3 paramètres (deux entiers et un opérateur, +, -, x, / ou %)
    //
    if (operator === "+"){ // créer une condition (méthode au choix) pour chaque valeur possible de l'opérateur
        return console.log (number+number1); // qui retournera le résultat du calcul 
-   } 
+   } else if (operator === "-"){
+       return console.log (number-number1);
+   }else if (operator === "*"){
+        return console.log (number*number1);
+    }else if (operator === "%"){
+        return console.log (number%number1);
+    }else if (operator === "/"){
+        return console.log (number/number1);
+    }
 }
+console.log(calculate(process.agrv[2],process.agrv[3],process.agrv[4]));
